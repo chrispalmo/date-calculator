d0a69f8f1da965cfe68864c95037cfbe738f65d7
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2021-06-05 19:21:33 +1000

    feat: add tests

--- dateDifference.py
+++ dateDifference.py
@@ -8,29 +8,24 @@ def daysInMonth(month, year):
     daysInFeb = 29 if isLeapYear(year) else 28
     return [31, daysInFeb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month-1]
 
-def isValidDateString(date):
-    "(date: any) => bool"
-    if (type(date) != str or len(date.split("/")) != 3):
-        return False
-    dayMonthYear = date.split("/")
-    day = dayMonthYear[0]
-    month = dayMonthYear[1]
-    year = dayMonthYear[2]
-    yearIsValid = type(year) == int
-    monthIsValid = type(month) == int & 0 <= month <= 12
-    dayIsValid = type(month) == int & 0 <= day <= daysInMonth(month, year)
-    return yearIsValid & monthIsValid & dayIsValid
-
 def parseDateString(dateString):
     "(dateString: any) => {day: int, month: int, year: int}"
-    if not isValidDateString(dateString):
-        raise ValueError("Invalid dateString: expected 'dd/mm/yyyy'")
-    dayMonthYear = dateString.split("/")
-    return {
-        "day": int(dayMonthYear[0]),
-        "month": int(dayMonthYear[1]),
-        "year": int(dayMonthYear[2]),
-    }
+    ERROR_MESSAGE = "Invalid dateString: expected 'dd/mm/yyyy'"
+    try:
+        dayMonthYear = dateString.split("/")
+        day = int(dayMonthYear[0])
+        month = int(dayMonthYear[1])
+        year = int(dayMonthYear[2])
+        if (0 < day <= daysInMonth(month, year)) & (0 < month <= 12):
+            return {
+                "day": day,
+                "month": month,
+                "year": year,
+            }
+        else:
+            raise ValueError(ERROR_MESSAGE)
+    except Exception:
+        raise ValueError(ERROR_MESSAGE)
 
 def daysFromEpoch(dateString):
     date = parseDateString(dateString)
@@ -40,12 +35,51 @@ def daysFromEpoch(dateString):
             for month in range(1, date["month"] + 1):
                 if month != date["month"]:
                     totalDays += daysInMonth(month, year)
-                    # print(f'Y: {year},\tM: {month},\tD: {daysInMonth(month, year)},\t D_total: {totalDays}')
         else:
             for month in range(1, 12 + 1):
                 totalDays += daysInMonth(month, year)
-                # print(f'Y: {year},\tM: {month},\tD: {daysInMonth(month, year)},\t D_total: {totalDays}')
     return totalDays
 
 def daysBetween(dateString1, dateString2):
     return daysFromEpoch(dateString2) - daysFromEpoch(dateString1)
+
+if __name__ == "__main__":
+    TEST_CASES = [
+        {"arg1": "02/06/1983", "arg2": "22/06/1983", "expectedResult": 19+1},
+        {"arg1": "04/07/1984", "arg2": "25/12/1984", "expectedResult": 173+1},
+        {"arg1": "03/01/1989", "arg2": "03/08/1983", "expectedResult": -1979-1},
+        {"arg1": "11/08/1990", "arg2": "11/08/1990", "expectedResult": "example of failing test"},
+        {"arg1": "11/08/1990", "arg2": "11/08/1990", "expectedResult": 0},
+        {"arg1": "11/08/1990", "arg2": "12/08/1990", "expectedResult": 1},
+        {"arg1": "11/08/1990", "arg2": "10/08/1990", "expectedResult": -1},
+        {"arg1": "00/01/1000", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "99/01/1000", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "-3/01/1000", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "01/00/1000", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "01/99/1000", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "01/-3/1000", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "11-11-1111", "arg2": "11/11/1111", "expectedResult": Exception},
+        {"arg1": "abcdefg", "arg2": "11/11/1111", "expectedResult": Exception},
+    ]
+
+    def printPass(testCase):
+        print(f'PASS:\t {testCase}')
+
+    def printFail(testCase, result):
+        print(f'\nFAIL:\t {testCase}\n')
+        print(f'expected: {testCase["expectedResult"]}')
+        print(f'received: {result}')
+        print("\n")
+
+    for testCase in TEST_CASES:
+        try:
+            result = daysBetween(testCase["arg1"], testCase["arg2"])
+            if result == testCase["expectedResult"]:
+                printPass(testCase)
+            else:
+                printFail(testCase, result)
+        except Exception as e:
+            if testCase["expectedResult"] == Exception:
+               printPass(testCase) 
+            else:
+                printFail(testCase, Exception)
